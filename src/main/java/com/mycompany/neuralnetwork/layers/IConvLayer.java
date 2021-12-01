48f2ff24028c70b107110baf4e0b746f05bf97be
Dmytro <dmytrodanylov@ukr.net>
2021-12-01 22:37:18 +0200

    created IConvLayer

---
+++ src/main/java/com/mycompany/neuralnetwork/layers/IConvLayer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.neuralnetwork.layers;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ *
+ * @author dmytr
+ */
+public interface IConvLayer extends Layer {
+    
+    INDArray[] backProp(INDArray nextWeights, INDArray prevActivations, INDArray prevDelta, int[][] deltasPositions);
+}
