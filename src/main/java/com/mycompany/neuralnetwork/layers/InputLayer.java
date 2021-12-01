06cd7e1fc4b8e23c322bc4d784e85b11656e513d
Dmytro <dmytrodanylov@ukr.net>
2021-12-01 22:38:39 +0200

    created InputLayer

---
+++ src/main/java/com/mycompany/neuralnetwork/layers/InputLayer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.neuralnetwork.layers;
+
+import com.mycompany.neuralnetwork.neuron.Neuron;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ *
+ * @author dmytr
+ */
+public class InputLayer implements Layer {
+    
+    INDArray activations;
+    
+    public InputLayer() {
+    }
+    
+    public INDArray feedforward(INDArray activations){
+        this.activations=activations;
+        return activations;
+    }
+    
+    public INDArray[] backProp(INDArray nextWeights, INDArray prevActivations, INDArray prevDelta){        
+        return null;
+    } 
+    
+    public INDArray getActivations(){
+        return activations;
+    }
+}
