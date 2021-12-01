907c0157bfc4d7089f940f5172d398d3df0b974f
Dmytro <dmytrodanylov@ukr.net>
2021-12-01 22:37:32 +0200

    created HiddenLayer

---
+++ src/main/java/com/mycompany/neuralnetwork/layers/HiddenLayer.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.neuralnetwork.layers;
+
+import com.mycompany.neuralnetwork.neuron.Neuron;
+import java.util.Random;
+import java.util.stream.DoubleStream;
+import org.nd4j.linalg.api.buffer.DataType;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+/**
+ *
+ * @author dmytr
+ */
+public abstract class HiddenLayer implements Layer {
+    
+    private Neuron neuron;
+    private INDArray z;
+    private INDArray weights;
+    private INDArray biases;
+
+    public HiddenLayer(Neuron neuron) {
+        this.neuron = neuron;
+    }
+    
+    public INDArray getActivations(){
+        return neuron.fun(z);
+    }
+        
+    public Neuron getNeuron() {
+        return neuron;
+    }
+
+    public INDArray getZ() {
+        return z;
+    }
+
+    public INDArray getWeights() {
+        return weights;
+    }
+
+    public INDArray getBiases() {
+        return biases;
+    }
+    
+    public void setNeuron(Neuron neuron) {
+        this.neuron = neuron;
+    }
+
+    public void setZ(INDArray z) {
+        this.z = z;
+    }
+
+    public void setWeights(INDArray weights) {
+        this.weights = weights;
+    }
+
+    public void setBiases(INDArray biases) {
+        this.biases = biases;
+    }
+}
