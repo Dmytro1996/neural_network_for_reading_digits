f18f059c11d41cd0d10ea06c2db5addc37819172
Dmytro <dmytrodanylov@ukr.net>
2021-12-01 22:38:14 +0200

    created SoftmaxLayer

---
+++ src/main/java/com/mycompany/neuralnetwork/layers/SoftmaxLayer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.neuralnetwork.layers;
+
+import com.mycompany.neuralnetwork.cost.Cost;
+import com.mycompany.neuralnetwork.cost.LogLikehoodCost;
+import com.mycompany.neuralnetwork.neuron.Neuron;
+import com.mycompany.neuralnetwork.neuron.SoftmaxNeuron;
+
+/**
+ *
+ * @author dmytr
+ */
+public class SoftmaxLayer extends OutputLayer {
+
+    public SoftmaxLayer(int nIn, int nOut) {
+        super(nIn, nOut, new SoftmaxNeuron(), new LogLikehoodCost());
+    }
+    
+}
