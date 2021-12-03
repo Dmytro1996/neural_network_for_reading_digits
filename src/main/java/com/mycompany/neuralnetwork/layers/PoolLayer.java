5c89b444568f902802c6397523108e32d1258259
Dmytro <dmytrodanylov@ukr.net>
2021-12-03 22:19:01 +0200

    changed logic in constructor to set height and width differenctly compared to ConvLayer

--- src/main/java/com/mycompany/neuralnetwork/layers/PoolLayer.java
+++ src/main/java/com/mycompany/neuralnetwork/layers/PoolLayer.java
@@ -8,6 +8,8 @@ package com.mycompany.neuralnetwork.layers;
 import com.mycompany.neuralnetwork.neuron.Neuron;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -19,8 +21,18 @@ import org.nd4j.linalg.factory.Nd4j;
  */
 public class PoolLayer extends ConvLayer{
 
-    public PoolLayer(int[] image_shape,int numOfLayers, int[] kernel, Neuron neuron) {
-        super(image_shape,numOfLayers, kernel, neuron);
+    public PoolLayer(int[] image_shape,int numOfFilters, int[] kernel, Neuron neuron) {
+        super(neuron);
+        setImage_shape(image_shape);
+        setNumOfFilters(numOfFilters);
+        setKernel(kernel);
+        setHeight(image_shape[1]/kernel[0]);
+        setWidth(image_shape[2]/kernel[1]);
+        Random rand=new Random();
+        setWeights(Nd4j.create(DoubleStream.generate(()->rand.nextGaussian()).limit(numOfFilters*kernel[0]*kernel[1]).toArray(),
+                new long[]{numOfFilters,1,1,kernel[0],kernel[1]}, DataType.DOUBLE));
+        setBiases(Nd4j.create(DoubleStream.generate(()->rand.nextGaussian()).limit(getNumOfFilters()*getWidth()*getHeight()).toArray(),
+                new long[]{numOfFilters*getWidth()*getHeight()}, DataType.DOUBLE));
     }
     
    public INDArray feedforward(INDArray activations){
